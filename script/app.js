3e0c800a8e22de40662f5ddb6a5fd1b59b0f249c
horex-pro <hosseinhorex@gmail.com>
2024-04-04 10:09:02 +0330

    bugs fixed

--- script/app.js
+++ script/app.js
@@ -1,17 +1,21 @@
 let randomNumber = Math.floor(Math.random() * 100);
-console.log(randomNumber)
-let userform = document.getElementsByTagName("form")[0];
-const intrednumber = document.getElementsByName('number')[0]
-console.log(intrednumber)
-userform.addEventListener("submit", (e)=> {
- let userNumber = e.target.intrednumber.value
+console.log(randomNumber);
+
+let userForm = document.getElementsByTagName("form")[0]; // invalid variable name. userForm not userform (camelCase)
+
+// const intrednumber = document.getElementsByName('number')[0] this line should be deleted. you don't need to select input
+
+userForm.addEventListener("submit", (e) => {
+  let userNumber = e.target.number.value; // instead of varibale's name, as i said you most write input name.
   switch (true) {
     case userNumber == randomNumber:
-      alert('you win')
+      alert("you win");
       break;
-  
+
     default:
       break;
   }
   e.preventDefault();
 });
+
+// up to here, some bugs fixed. do it by yourself
